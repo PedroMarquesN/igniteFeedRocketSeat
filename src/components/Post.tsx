0c1e9d1fa557c55edb65837bcbc2e1b6eef3fdfe
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-14 15:23:40 -0300

    Style : Finish componente POST

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -27,8 +27,17 @@ export function Post() {
                     <p> 👉{' '}<a href="#"> jane.design/doctorcare</a></p>
 
                     <p> <a href="#"> #novoprojeto #nlw #rocketseat</a> </p>
-
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder="Deixe um comentario" />
+                <footer>
+                    <button type="submit">Comentar</button>
+                </footer>
+
+            </form>
         </article>
     )
 }
